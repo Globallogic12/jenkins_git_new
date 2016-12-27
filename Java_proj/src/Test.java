0a6ca7e465fc815426337c5144fb56b533a43cb2
garima1212 <garima.mehra12@yahoo.com>
2016-12-27 11:25:43 +0530

    Add files via upload

---
+++ Java_proj/src/Test.java
@@ -0,0 +1,9 @@
+class Test {
+	  public static void main(String[] arguments) {
+	    int c; //declaring a variable
+	 	 
+	    for (c = 1; c <= 30; c++) {
+	      System.out.println(c);
+	    }
+	  }
+	}
