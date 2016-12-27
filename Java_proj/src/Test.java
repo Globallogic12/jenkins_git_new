aa77ce066bf1ebd9e34c1f48eb33c1f4ba68295b
garima1212 <garima.mehra12@yahoo.com>
2016-12-27 11:21:09 +0530

    Delete Test.java

--- Java_proj/src/Test.java
+++
@@ -1,9 +0,0 @@
-class Test {
-	  public static void main(String[] arguments) {
-	    int c; //declaring a variable
-	 	 
-	    for (c = 1; c <= 30; c++) {
-	      System.out.println(c);
-	    }
-	  }
-	}
